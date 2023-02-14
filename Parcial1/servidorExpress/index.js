fe25cdf61f26accd1bc8def5f3c47179f13ceed8
JesusGuMe <gutierrezmjesus6@gmail.com>
2023-02-14 08:47:59 -0600

    Se le agrega el modulo de CORS al servidor express

--- Parcial1/servidorExpress/index.js
+++ Parcial1/servidorExpress/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json())
 app.use(express.text())
+app.use(cors())
+app.use(morgan('tiny'))
 
 app.get('/alumnos/:carrera', (req,res) => {
     //console.log(req.params.carrera)
