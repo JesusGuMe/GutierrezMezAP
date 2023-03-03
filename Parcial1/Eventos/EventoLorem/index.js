b220653710f07b63fcbb86bc76598e2dda0e10d8
JesusGuMe <gutierrezmjesus6@gmail.com>
2023-03-03 08:25:15 -0600

    Actualización.

--- Parcial1/Eventos/EventoLorem/index.js
+++ Parcial1/Eventos/EventoLorem/index.js
@@ -2,21 +2,17 @@ let path = require('path');
 let fs = require('fs');
 let archivoLorem = path.join(__dirname, 'Lorem.txt');
 
-// Se usa createReadStream para leer el archivo Lorem, y después, parecido a lo visto anteriormente, 
-// cada vez que se haga cambios en el archivo Lorem.txt y se guarden, se registraran en el OUTPUT.
-reader = fs.createReadStream(archivoLorem);
+const streamLectura = fs.createReadStream(archivoLorem, 'utf-8');
  
-reader.on('data', function (chunk) {
+streamLectura.on('data', function (chunk) {
   
   console.log(chunk.toString()); // Muestra todo el contenido del Lorem.txt en el OUTPUT.
   console.log("Se ha ejecutado el evento para leer el Lorem")
  
 });
 
-lor = fs.watch(archivoLorem, function(evento, archivo) {
-  console.log(`Sucedio el evento ${evento} en el archivo ${archivo}`)
-});
-
-lor.on('change', () => {
-  console.log('Se cambio el Archivo')
+streamLectura.on('error', function (error) {
+  console.log(`Ocurrio un Error: ${error.message}`);
+  console.log('')
+  console.log(error)
 })
